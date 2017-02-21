2dfcda9c081d662e151025ad4eafe9400d240118
Jessica Leach <jessicamaryleach@gmail.com>
2017-02-21 15:48:10 +0100

    comments added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//declare npm packages
 const express = require('express'),
       morgan = require('morgan'),
       bodyParser = require('body-parser'),
@@ -5,42 +6,74 @@ const express = require('express'),
       pug = require('pug'),
       Sequelize = require('sequelize');
 
+// declaring that we are using express
 var app = express(),
+//connecting to 'bulletinboard' database, accessing 'process.env.POSTGRES_USER' from .bash_profile
     sequelize = new Sequelize('bulletinboard', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, { dialect: 'postgres' });
 
+//requiring route, notices.js in 'routes' folder
 var noticesRouter = require('./routes/notices');
 
+//creates the table, 'notices'
+//come back to 'Why does it add an s in the table name?'
 var notice = sequelize.define('notice', {
     title: Sequelize.STRING,
     body: Sequelize.TEXT
 });
 
+//use morgan middleware to print HTTP requests to help with debugging
 app.use(morgan('dev'));
 
+//In order to parse a text input from a user, we need bodyParser to convert the data to a JavaScript string
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//setting the view engine as pug - pug files render to html
 app.set('view engine', 'pug');
 
+//use noticesRouter variable
+app.use('/add-notice', noticesRouter);
+
+//get request to redirect the 'homepage' to the notices route
+
+//1. go to route notices.js, go to the app.get('/') in the
+//notices.js file
 app.get('/', (request, response) => {
-  response.redirect('/notices');
+  response.redirect('/add-notice');
 });
 
+//get request to /board route
 app.get('/board', (request, response) => {
+  //Promise to findAll - native method in sequelize, findAll returns
+  //everything in the notice table
+  //'notices' is passed as a parameter
   notice.findAll().then((notices) => {
+    //renders the board.pug file, in the notices folder
+    //notices object is passed as a parameter, to be rendered in pug file
     response.render('notices/board', { notices: notices });
   });
 });
 
-app.post('/notices', (request, response) => {
+//insert a new entry into the notices table
+app.post('/new-notice', (request, response) => {
+
+  console.log(request.body.id);
+  console.log(request.body.title);
+  console.log(request.body.body);
+  console.log(request.body);
+  //takes everthing in request.body, from name attribute in pug file
+  //name="title" and name="body"
+  //create is native to sequlelize, creates a new entry into notices
+  //table with a promise
   notice.create(request.body).then(() => {
+    //redirects to url [home]/board, goes to /board get request on line 45 here
     response.redirect('/board');
   });
 });
 
-app.use('/notices', noticesRouter);
-
+//promise - connects to database with sequelize
 sequelize.sync().then(() => {
   console.log('Connected to database');
+  //creates a server
   app.listen(3000, () => {
     console.log('Web Server is running on port 3000');
   });
--- routes/notices.js
+++ routes/notices.js
@@ -1,27 +1,31 @@
+//requiring npm modules
 const express = require('express'),
-      Sequelize = require('Sequelize'),
-      router = express.Router();
+      Sequelize = require('Sequelize');
 
+//initiating the router - setting express as the router
+const router = express.Router();
+
+//connecting to the bulletinboard database through sequelize,  process.env.POSTGRES_USER finds username from .bash_profile
 const sequelize = new Sequelize('bulletinboard', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, { dialect: 'postgres' });
-
 
 var notice = sequelize.define('notice', {
   title: Sequelize.STRING,
   body: Sequelize.TEXT
 });
 
+//get request to render the index.pug file - to add a new notice
+//why do we use 'router.get' and not 'app.get'?
 router.get('/', (request, response) => {
-  notice.findAll({ order: 'id ASC' }).then((notices) => {
-    response.render('notices/index', { notices: notices });
-  });
+    response.render('notices/index');
 });
 
-
+//gets the unique id,
 router.get('/:id', (request, response) => {
+  //find the full row of data in 'notices' table from the id
   notice.findById(request.params.id).then((notice) => {
+    //renders the show.pug file
     response.render('notices/show', { notice: notice });
   });
 });
-
 
 module.exports = router;
